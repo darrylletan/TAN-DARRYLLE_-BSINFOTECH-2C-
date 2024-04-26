225d2cf1ecd809c5e17d12db2fa985b90162c38b
darrylletan <134573430+darrylletan@users.noreply.github.com>
2024-04-26 13:34:50 +0800

    number-challenge.js

---
+++ number-challenge.js
@@ -0,0 +1,30 @@
+/*
+    1 Create a function that will return the sum 3 numbers
+    2 Create a function that will return the difference of the 2 numbers
+    3 Create a function that will return the product of the 2 numbers
+    4 Create a function that will return the average of the 2 numbers
+*/
+
+*// 1.
+  function sumThreeNumbers(a, b, c) {
+    return a + b + c;
+}
+console.log(sumThreeNumbers(2, 3, 4)); // Output: 9
+
+
+*// 2.
+function differenceOfTwoNumbers(a, b) {
+    return a - b;
+}
+
+
+*// 3.
+function productOfTwoNumbers(a, b) {
+    return a * b;
+}
+
+
+*// 4.
+function averageOfTwoNumbers(a, b) {
+    return (a + b) / 2;
+}
